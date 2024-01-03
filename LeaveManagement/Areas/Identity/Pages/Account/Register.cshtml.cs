2ef19b6451fa27e9015d63021708e6eb0981fce1
Neo Tshoma <neo.tshoma@tangentsolutions.co.za>
2024-01-03 16:19:03 +0200

    Updated the auth and added roles

--- LeaveManagement/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LeaveManagement/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using LeaveManagement.Constants;
 
 namespace LeaveManagement.Areas.Identity.Pages.Account
 {
@@ -142,11 +143,12 @@ namespace LeaveManagement.Areas.Identity.Pages.Account
                 user.DateOfBirth = Input.DateOfBirth;
                 user.TaxId = new Random(12).ToString();
 
-                var result = await _userManager.CreateAsync(user, Input.Password);
+                var result = await _userManager.CreateAsync(user, Input.Password);  
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, Roles.User);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
